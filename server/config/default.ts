6fa89d0b014523411f33e6b3ec42496a883471cc
NikolozKurtanidze <nikoloz.kurtanidze@yandex.com>
2023-01-25 20:48:28 +0400

    added discord bot cmd

--- server/config/default.ts
+++ server/config/default.ts
@@ -2,4 +2,7 @@ export default {
     corsOrigin: "http://localhost:3000",
     port: process.env.PORT ?? 4000,
     host: "https://api.kiuvinme.ge",
+    botToken: process.env.BOT_TOKEN ?? "MTA2NzcxNDQxMzk2NjE1MTcwMQ.GlxXnp.zjJS2nCBLlrHUMzoFxdl8VzVDSwhjnZDHmXiNs",
+    discordServerId: process.env.DISCORD_SERVER_ID ?? "1067715386117738527",
+    counterChannelId: process.env.COUNTER_CHANNEL_ID ?? "1067729935176831057",
 };
