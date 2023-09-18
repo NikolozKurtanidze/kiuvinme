3877a73cf0af90432e475edc8ee87332919ba5fa
NikolozKurtanidze <nikoloz.kurtanidze@yandex.com>
2023-09-18 19:50:33 +0400

    add gtag info

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -21,6 +21,21 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>{CssBaseline.flush()}</Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTAG_ID}`}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '${process.env.NEXT_PUBLIC_GTAG_ID}');
+  `,
+          }}
+        />
         <body>
           <Main />
           <NextScript />
